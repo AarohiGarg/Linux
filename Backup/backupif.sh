a3bd1b10853f9519111909de6bc519b1891048b9
Harshilbhardwaj47 <edwardkenway200017@gmail.com>
2020-04-25 19:21:58 -0700

    backup script using if else

--- Backup/backupif.sh
+++ Backup/backupif.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-
-user=$(whoami)
+if [ -z $1 ]; then                                                                                                                                                                                                                                                             
+        user=$(whoami)                                                                                                                                                                                                                                                         
+else                                                                                                                                                                                                                                                                           
+        if [ ! -d "/home/$1" ]; then                                                                                                                                                                                                                                           
+                echo "Requested $1 user home directory doesn't exist."                                                                                                                                                                                                         
+                exit 1                                                                                                                                                                                                                                                         
+        fi                                                                                                                                                                                                                                                                     
+        user=$1                                                                                                                                                                                                                                                                
+fi            
 input=/home/$user
 output=/tmp/${user}_home_$(date +%Y-%m-%d_%H%M%S).tar.gz
 
